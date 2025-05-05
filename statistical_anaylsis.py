0aeecc70968316ccc4472082c801d1196c9433a3
cognitive <maya16800@gmail.com>
2025-05-05 21:12:10 +0300

    minor changes- had not been in use

--- statistical_anaylsis.py
+++ statistical_anaylsis.py
@@ -3,23 +3,23 @@ import numpy as np
 
 
 df = pd.read_csv("D:/seizures_analysis/output/clean_final_table.csv")
-metadata_cols = ["pat_num",	"classif.",	"FBTCS_code",	"vigilance",	"vigilance_code",	"lobe",	"temporal_code",	"side"]
+metadata_cols = ["pat_num",	"classif.",	"FBTCS_code",	"vigilance", "vigilance_code", "lobe",	"temporal_code", "side"]
 
 feature_cols = ['T7_delta_gamma_ratio', 'T7_sigma_beta_ratio',
                 'T8_delta_gamma_ratio', 'T8_sigma_beta_ratio',
                 'PZ_delta_gamma_ratio', 'PZ_sigma_beta_ratio',
                 'FZ_delta_gamma_ratio', 'FZ_sigma_beta_ratio']
 
-df = df[metadata_cols + feature_cols]
+df = df[metadata_cols + feature_cols]  # removing unnecessary columns
 
 th = df[feature_cols].quantile(0.95)
 
 for feature in feature_cols:
 
-    max_value = th[feature]
-    df = df[df[feature] < max_value ]
-    print(df.groupby('FBTCS_code').count()['FZ_sigma_beta_ratio'])
-    print(df[feature].min())
+    max_value = df[th]
+    df = df[df[feature] < max_value]
+    # print(df.groupby('FBTCS_code').count()['FZ_sigma_beta_ratio'])
+    # print(df[feature].min())
     df[f"log_{feature}"] = np.log(df[feature] + 1)
 
 df.to_csv("D:/seizures_analysis/output/clean_final_table.csv")
